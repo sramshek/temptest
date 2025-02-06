1ef98fe0b5e473fa718e2cd733aa79093a73e509
Sachin Upadhyay <79317714+sramshek@users.noreply.github.com>
2025-02-06 13:04:43 +0530

    direct upload
    uploaded from github

---
+++ project1-project1/src/main/resources/.sw-python.py
@@ -0,0 +1,9 @@
+{
+  "version" : "1",
+  "artifactType" : {
+    "name" : "Python Script",
+    "category" : "managed file"
+  },
+  "attributes" : { },
+  "creationTimeMilliseconds" : 1738827214754
+}
