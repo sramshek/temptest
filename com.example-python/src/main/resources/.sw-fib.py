1dc8631a7940f3994b8b2e6b36e0109ad2d0d15e
sramshek <>
2025-02-11 20:59:09 +0530

    Checkpoint-Name: chekc1

---
+++ com.example-python/src/main/resources/.sw-fib.py
@@ -0,0 +1,9 @@
+{
+  "version" : "1",
+  "artifactType" : {
+    "name" : "Python Script",
+    "category" : "managed file"
+  },
+  "attributes" : { },
+  "creationTimeMilliseconds" : 1739287728740
+}
---
+++ com.example-python/src/main/resources/fib.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+def fib2(n): # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)    # see below
+        a, b = b, a+b
+    return result
+
+fibresultFromFile = fib2(fib)
+print(fibresultFromFile)
+print('')
